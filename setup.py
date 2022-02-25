53565d02cb87e1fa5bb6aee88c3178c0c2875946
JarbyDaniel <dsalazarg@unicauca.edu.co>
2022-02-24 22:07:13 -0500

    nombre

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def package_data(pkg, roots):
 
 
 setup(
-    name='adaptive_test-xblock',
+    name='adaptive_test_base-xblock',
     version='1.1',
     description='adaptive_test XBlock',   # TODO: write a better description.
     license='UNKNOWN',          # TODO: choose a license: 'AGPL v3' and 'Apache 2.0' are popular.
